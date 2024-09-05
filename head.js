2b7389de94435bc591900ce5c2245d83024dc66c
N!no <ninomyemail@gmail.com>
2024-09-05 16:27:41 -0400

    i

---
+++ head.js
@@ -0,0 +1,34 @@
+document.write(`
+        <p style="color: #224b8d;
+                  margin-top: 0.7em;
+                  margin-bottom: 0.5em;
+                  padding-bottom: 0.4em;
+                  line-height: 1.5;
+                  padding-top: 0.5em;
+                  border-bottom: 2px solid #aaaaaa;
+                  font-size: 180%;">
+        <b>Shuo Liu</b>
+        <span style="float:right; font-family:Verdana;">
+            <top-bar><a href="./index.html">Home</a> | <a href="./research.html">Research</a> | <a href="./publications.html">Publications</a> | <a href="./software.html">Softwares</a></top-bar>
+        </span>
+        </p>
+
+        <p><img src="./imgs/proto2.png" height=180 align="right" style="padding: 0px 0px 0px 0px;" />
+            <b>Incoming Ph.D. Student</b> <br> <br> 
+            <a href = "https://llpr.ccs.neu.edu/"><em>Lab for Learning and Planning in Robotics</em></a><br>
+            <a href = "https://www.khoury.northeastern.edu/research/"><em>Khoury College of Computer Sciences<br>Northeastern University</em><br></a>
+        </p>
+        805 Columbus Ave<br>
+        Boston, MA, 02120<br>
+        liu [<font style="font-family:'Schoolbell'">dot</font>] shuo2 [<font style="font-family:'Schoolbell'">at</font>] northeastern [<font style="font-family:'Schoolbell'">dot</font>] edu
+        <br>
+        <br>
+
+        <a href = "https://scholar.google.co.uk/citations?user=85_4lIYAAAAJ&hl"><img src="./imgs/icon/google-scholar.svg" class="filter-svg-blue" height="24"/></i></a> &nbsp
+        <a href = "https://github.com/LovelyBuggies"><img src="./imgs/icon/github.svg" class="filter-svg-blue" height="28"></i></a> &nbsp
+        <a href = "https://twitter.com/ShuoLiu14"><img src="./imgs/icon/x-twitter.svg" class="filter-svg-blue" height="28"></i></a> &nbsp
+        <a href = "https://orcid.org/my-orcid?orcid=0000-0003-0177-6102"><img src="./imgs/icon/orcid.svg" class="filter-svg-blue" height="28"></i></a> &nbsp
+        <a href = "https://www.researchgate.net/profile/Shuo-Liu-96"><img src="./imgs/icon/researchgate.svg" class="filter-svg-blue" height="28"></i></a> &nbsp
+        <!-- https://fontawesome.com/icons -->
+
+`);
