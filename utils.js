c8204d13dd3a669f35c49df081b4434fbf782815
N!no <ninomyemail@gmail.com>
2024-11-13 22:32:22 -0500

    update tags

---
+++ utils.js
@@ -0,0 +1,65 @@
+function rlSortList(sortBy) {
+    sortList('rl-post-list', sortBy);
+}
+
+function marlSortList(sortBy) {
+    sortList('marl-post-list', sortBy);
+}
+
+function sortList(listId, sortBy) {
+    const list = document.getElementById(listId);
+    const items = Array.from(list.getElementsByTagName("li"));
+
+    items.sort((a, b) => {
+        if (sortBy === "date") {
+            const dateA = new Date(a.dataset.date);
+            const dateB = new Date(b.dataset.date);
+            return dateB - dateA;
+        } else if (sortBy === "depth") {
+            const depthA = parseInt(a.dataset.depth);
+            const depthB = parseInt(b.dataset.depth);
+            return depthA - depthB;
+        }
+    });
+
+    list.innerHTML = "";
+    items.forEach(item => list.appendChild(item));
+}
+
+function showTaggedItems(tag) {
+    const lists = ['rl-post-list', 'marl-post-list'];
+    const filteredList = document.getElementById("filtered-list");
+
+    filteredList.innerHTML = "";
+
+    lists.forEach(listId => {
+        const list = document.getElementById(listId);
+        const items = Array.from(list.getElementsByTagName("li"));
+
+        items.forEach(item => {
+            const tags = item.dataset.tag ? item.dataset.tag.split(',') : [];
+            if (tags.includes(tag)) {
+                const clone = item.cloneNode(true);
+                filteredList.appendChild(clone);
+            }
+        });
+    });
+}
+
+function addTagAndDateInfo() {
+    const lists = document.querySelectorAll("#rl-post-list li, #marl-post-list li");
+
+    lists.forEach(item => {
+        const annotateElement = item.querySelector("annotate");
+        const date = item.getAttribute("data-date");
+        const tags = item.getAttribute("data-tag") || "NULL";
+        const infoElement = document.createElement("span");
+        infoElement.style.fontSize = "0.8em"; 
+        infoElement.style.color = "#888888";
+        infoElement.style.fontStyle = "italic";
+        infoElement.innerHTML = `Tags: ${tags}; Date: ${date}.`;
+        annotateElement.insertAdjacentHTML("afterend", "<br>");
+        annotateElement.insertAdjacentElement("afterend", infoElement);
+        annotateElement.insertAdjacentHTML("afterend", "</br>");
+    });
+}
