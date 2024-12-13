821b9a55a0a83c93db43188bdde5f7fc13063f61
N!no <ninomyemail@gmail.com>
2024-12-13 17:45:31 -0500

    u

--- head.js
+++ head.js
@@ -9,7 +9,7 @@ document.write(`
                   font-size: 180%;">
         <b>Shuo Liu</b>
         <span style="float:right;">
-            <top-bar><a href="./index.html">Home</a> | <a href="./research.html">Research</a> | <a href="./publications.html">Publications</a> | <a href="./posts.html">Posts</a> | <a href="./software.html">Softwares</a></top-bar>
+            <top-bar><a href="./index.html">Home</a> | <a href="./research.html">Research</a> | <a href="./publications.html">Publications</a> | <a href="./posts.html">Posts</a> | <a href="./software.html">Softwares</a> | <a href="./philos.html">Philos</a></top-bar>
         </span>
         </p>
 
